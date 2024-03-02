a826e28d5817e3bbef0b4f926dd5a91db5790908
srikarboga <boggavarapu.srikar@gmail.com>
2024-03-01 22:37:35 -0500

    added accurate timer using date module

--- counter/src/CountingComponent.jsx
+++ counter/src/CountingComponent.jsx
@@ -1,15 +1,28 @@
 import styles from './App.module.css';
-import { createSignal, onCleanup } from "solid-js";
+import { createEffect, createSignal, onCleanup } from "solid-js";
 
 const CountingComponent = () => {
+	const [start, setStart] = createSignal(Date.now());
 	const [count, setCount] = createSignal(0);
+	const [time, setTime] = createSignal(Date.now());
 	const interval = setInterval(
 		() => setCount(c => c + 1),
 		1000
 	);
+	
+	createEffect(() => {
+		const timer = setInterval(
+			() => setTime(Date.now()),
+			100
+		);
+		onCleanup(() => clearInterval(timer))
+	})
 	onCleanup(() => clearInterval(interval));
-	return <div class={styles.App}> <header class={styles.header}>Count value is {count()}</header></div>;
+	return <div class={styles.App}> 
+	<header class={styles.header}>Count value is {count()}
+	<p>Timer: {((time() - start())/1000).toFixed(1)}</p>
+	</header>
+	</div>;
 };
-
 
 export default CountingComponent;
